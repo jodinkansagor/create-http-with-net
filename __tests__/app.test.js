b607c4a238062212b8235ea2f32894ae3274ba9a
Jodi N Kansagor <joedvice@gmail.com>
2019-12-09 15:20:45 -0800

    fixed post path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,5 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-  it('gives us the correct response when given type and path', () => {
-    const request = { method: 'GET', path: '/' };
 
-    
-  })
 });
