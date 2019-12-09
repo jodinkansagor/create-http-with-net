401ea7d482aaca161ee9e98de803251dd46a92f4
Jodi N Kansagor <joedvice@gmail.com>
2019-12-09 14:41:21 -0800

    wrote paths

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('gives us the correct response when given type and path', () => {
+    const request = { method: 'GET', path: '/' };
 
+    
+  })
 });
